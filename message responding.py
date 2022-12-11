9ac668d898b0bb60656882d15032d98e36355e78
Johnny <119725624+J0hnny13@users.noreply.github.com>
2022-12-11 22:12:12 +0700

    add manual
    made it easier to read
    
    Signed-off-by: Johnny <119725624+J0hnny13@users.noreply.github.com>

--- message responding.py
+++ message responding.py
@@ -7,11 +7,15 @@ intents.members = True
 intents.message_content = True
 client = commands.Bot(command_prefix= '$', intents = intents)
 
+
+#Bot standby
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
+
 #Send private message to new members
+
 @client.event
 async def on_member_join(member):
     await member.create_dm()
@@ -19,24 +23,26 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server!'
     )
 
-#Responding to message
+
+#Sever message responding
+
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user: #Client check (prevent responding to itself)
         return
-    if 'hello' in message.content.lower():
+    if 'hello' in message.content.lower(): #respond to 'hello' in chat
         await message.add_reaction('\U0001F618')
         await message.channel.send(f'Good morning!, {message.author.display_name}')
 
-    if 'goodbye' in message.content.lower():
+    if 'goodbye' in message.content.lower(): #respond to 'goodbye' in chat
         await message.channel.send(f'See you later!, {message.author.display_name}')
 
-    if 'happy birthday' in message.content.lower():
+    if 'happy birthday' in message.content.lower(): #respond to 'happy birthday' in chat
         await message.add_reaction('\U0001F973')
         await message.channel.send('Happy Birthday! 🎈🎉')
 
-    if 'dolph' in message.content.lower():
+    if 'dolph' in message.content.lower(): #little easter egg
         await message.add_reaction('\U0001F42C')
 
 
-client.run('')
+client.run('put token here')
