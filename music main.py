3c0124db3c7b27066ce73460ac240279dfc9ea52
Bobby715623 <109953502+Bobby715623@users.noreply.github.com>
2022-12-13 17:58:09 +0700

    music pre lastest
    Signed-off-by: Bobby715623 <109953502+Bobby715623@users.noreply.github.com>

---
+++ music main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+
+#import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+
+intents = discord.Intents.default()
+
+intents.members = True
+intents.message_content = True
+client = commands.Bot(command_prefix= '$', intents = intents)
+
+
+#remove the default help command so that we can write out own
+client.remove_command('help')
+
+#register the class with the bot
+client.add_cog(help_cog(client))
+client.add_cog(music_cog(client))
+
+#start the bot with our token
+client.run('your token')
